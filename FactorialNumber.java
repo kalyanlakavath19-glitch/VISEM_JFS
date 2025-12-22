13f236caecb149bebfc902179d0b8021e9e089a4
kalyanlakavath19-glitch <kalyanlakavath19@gmail.com>
2025-12-22 22:12:54 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,22 @@
+public class FactorialNumber {
+    //to print a given number factorial
+    //using Recusive method
+    //Recursive means ... without using for loop
+    //also the function calls itself
+    //i)Base - caase,, ii)Recursive -  case
+    public static int factorialNumber(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * factorialNumber(n - 1);
+    }    
+        public static void main(String args[]){
+            int num = 5;
+            int result = factorialNumber(num);
+            System.out.println(result);
+        }
+
+
+    
+    
+}
